8483aac554efb9a51fb0990f9b3285a54614537e
sergey.i.smirnov@rt.ru <78102903+MrXides@users.noreply.github.com>
2023-02-17 18:54:02 +0300

    UPD

--- src/main/java/ru/moonshine1l/mathtask/Controller.java
+++ src/main/java/ru/moonshine1l/mathtask/Controller.java
@@ -24,55 +24,43 @@ public class Controller {
     private Label task;
 
     @FXML
-    void initialize(){
-        classSelection.getItems().setAll(
-                1,
-                2,
-                3,
-                4);
+    void initialize() {
+        classSelection.getItems().setAll(1, 2, 3, 4);
+        generate.setOnAction(event -> {
 
-        Integer valueClass = classSelection.getValue();
-
-
-        generate.setOnAction( new EventHandler<ActionEvent>(){
-
-            @Override
-            public void handle(ActionEvent event) {
-                
-                switch (classSelection.getValue()) {
-                    case (1):
-                        task.setText(firstTask(0,50));
-                        break;
-                    case (2):
-                        task.setText(firstTask(0,100));
-                        break;
-                    case(3):
-                        task.setText(secondTask(0, 500));
-                        break;
-                    case(4):
-                        task.setText(secondTask(0,1000));
-                        break;
-                }
-            }});
-
-
+            switch (classSelection.getValue()) {
+                case (1):
+                    task.setText(firstTask(0, 50));
+                    break;
+                case (2):
+                    task.setText(firstTask(0, 100));
+                    break;
+                case (3):
+                    task.setText(secondTask(0, 500));
+                    break;
+                case (4):
+                    task.setText(secondTask(0, 1000));
+                    break;
+            }
+        });
     }
+
     public int getRandomNumber(int min, int max) {
         return (int) ((Math.random() * (max - min)) + min);
     }
 
-    public String firstTask(int min, int max){
-        String [] operator = {"+", "-"};
+    public String firstTask(int min, int max) {
+        String[] operator = {"+", "-"};
         Random random = new Random();
         int select = random.nextInt(operator.length);
-        return getRandomNumber(min,max) + operator[select] + (getRandomNumber(min,max)) + "=";
+        return getRandomNumber(min, max) + operator[select] + (getRandomNumber(min, max)) + "=";
     }
 
-    public String secondTask(int min, int max){
-        String [] operator = {"+", "-", "*", ":"};
+    public String secondTask(int min, int max) {
+        String[] operator = {"+", "-", "*", ":"};
         Random random = new Random();
         int select = random.nextInt(operator.length);
-        return getRandomNumber(min,max) + operator[select] + (getRandomNumber(min, max)) + "=";
+        return getRandomNumber(min, max) + operator[select] + (getRandomNumber(min, max)) + "=";
     }
 }
 
