02b5e777b5f5633f611e23e60c365919db8341d9
moonshine1l <norka5imen@gmail.com>
2023-02-18 10:38:07 +0300

    fix

--- src/main/java/ru/moonshine1l/mathtask/Controller.java
+++ src/main/java/ru/moonshine1l/mathtask/Controller.java
@@ -2,13 +2,14 @@ package ru.moonshine1l.mathtask;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.event.EventType;
 import javafx.fxml.FXML;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 
+import java.io.IOException;
 import java.util.Random;
 
 public class Controller {
@@ -23,99 +24,54 @@ public class Controller {
     private Label task;
 
     @FXML
-    private Label message;
+    void initialize() {
+        classSelection.getItems().setAll(1, 2, 3, 4);
+        generate.setOnAction(event -> {
 
-    @FXML
-    private TextField answer;
-
-    @FXML
-    private Button checkAnswer;
-
-    @FXML
-    void initialize(){
-        classSelection.getItems().setAll(
-                1,
-                2,
-                3,
-                4);
-
-        Integer valueClass = classSelection.getValue();
-
-
-
-        generate.setOnAction( new EventHandler<ActionEvent>(){
-
-            @Override
-            public void handle(ActionEvent event) {
-                
-                switch (classSelection.getValue()) {
-                    case (1):
-                        task.setText(firstTask(0,50));
-                        break;
-                    case (2):
-                        task.setText(String.valueOf(firstTask(0,100)));
-                        break;
-                    case(3):
-                        task.setText(secondTask(0, 250, 1, 10));
-                        break;
-                    case(4):
-                        task.setText(secondTask(0,500,1,10));
-                        break;
-
-                }
-
-            }});
-
-        checkAnswer.setOnAction((ActionEvent event) -> {
-                if(Integer.parseInt(task.getText()) == Integer.parseInt(answer.getText())){
-                    message.setText("Верно!)");
-                }else {
-                    message.setText("Ты ошибся(");
-                }
+            switch (classSelection.getValue()) {
+                case (1):
+                    task.setText(firstTask(0, 50));
+                    break;
+                case (2):
+                    task.setText(firstTask(0, 100));
+                    break;
+                case (3):
+                    task.setText(secondTask(0, 250, 1,10));
+                    break;
+                case (4):
+                    task.setText(secondTask(0, 500, 1, 10));
+                    break;
+            }
         });
-
     }
+
     public int getRandomNumber(int min, int max) {
         return (int) ((Math.random() * (max - min)) + min);
     }
 
-    public String firstTask(int min, int max){
-        String [] operator = {"+", "-"};
+    public String firstTask(int min, int max) {
+        String[] operator = {"+", "-"};
         Random random = new Random();
         int select = random.nextInt(operator.length);
-        int taskoper = 0;
-        return getRandomNumber(min,max) + operator[select] +  getRandomNumber(min,max);
+        return getRandomNumber(min, max) + operator[select] + (getRandomNumber(min, max));
+    }
 
-        if(operator[select].equals("+")){
-            taskoper = getRandomNumber(min,max) + getRandomNumber(min,max);
-        }else if(operator[select].equals("-")) {
-            taskoper = getRandomNumber(min, max)  -  (getRandomNumber(min, max));
+    public String secondTask(int min, int max, int secondMin, int secondMax) {
+        String[] operator = {"+", "-", "*", ":"};
+        Random random = new Random();
+        int select = random.nextInt(operator.length);
+        String result = null;
+        if (operator[select] == "+"){
+            result = getRandomNumber(min, max) + operator[select] + (getRandomNumber(min,max));
+        }else if(operator[select] == "-"){
+            result = getRandomNumber(min, max) + operator[select] + (getRandomNumber(min,max));
+        }else if(operator[select] == "*"){
+            result = getRandomNumber(min, max) + operator[select] + (getRandomNumber(secondMin,secondMax));
+        }else if(operator[select] == ":"){
+            result = getRandomNumber(min, max) + operator[select] + (getRandomNumber(secondMin,secondMax));
         }
-        return String.valueOf(taskoper);
+        return result;
     }
-
-    public String secondTask(int min, int max, int smin, int smax){
-         String[] operator = {"+", "-", "*", ":"};
-        Random random = new Random();
-        int select = random.nextInt(operator.length);
-
-        return getRandomNumber(min,max) + operator[select] + (getRandomNumber(smin, smax)) + "=";
-    }
-
-//    public int checkFirstResult(){
-//        String [] operator = {"+", "-"};
-//        Random random = new Random();
-//        int select = random.nextInt(operator.length);
-//        int taskoper = 0;
-//        String result = getRandomNumber() + operator[select] +  getRandomNumber();
-//
-//        if(operator[select].equals("+")){
-//            taskoper = getRandomNumber() + getRandomNumber();
-//        }else if(operator[select].equals("-")) {
-//            taskoper = getRandomNumber()  -  (getRandomNumber());
-//        }
-//        return taskoper;
-//    }
+    
 }
-
 
